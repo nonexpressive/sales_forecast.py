8078403cc5fed87fe38a2e22dfe19821f2dd0a54
nonexpressive <livjaxn@gmail.com>
2026-05-30 12:29:32 -0500

    sales_forecast.py

--- sales_forecast.py
+++ sales_forecast.py
@@ -117,6 +117,8 @@ def tabs():
     with tab2:
         st.header('Forecasts')
         forecast_2026_sales()
+              st.write('Sales forecasts are based on 2025 sales data.')
+          
 
 get_input()
 tabs()
